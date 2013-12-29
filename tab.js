ffcf7102f9a802db0eac2ae9db60f124edb80ba4
rleemanmunk <rayleemamunk@gmail.com>
2013-12-28 21:18:17 -0500

    Additions to tab container

--- tab.js
+++ tab.js
@@ -1,13 +1,32 @@
-require(["dijit/layout/TabContainer", "dijit/layout/ContentPane", "dojo/domReady!"], function(TabContainer, ContentPane){
-    var tc = new TabContainer({
+require([
+	"dijit/layout/TabContainer", 
+	"dijit/layout/ContentPane", 
+	"dijit/form/Textarea",
+	"dijit/form/Button",
+	"dojo/domReady!"
+	], function(TabContainer, ContentPane, Textarea, Button){
+    var textarea = new Textarea({
+		name: "code",
+		value: "Enter code here",
+		rows: "10",
+		cols: "30"
+	});
+	var runButton = new Button({
+        label: "RUN!",
+        onClick: function(){
+            // Do something:
+        }
+	});
+	var tc = new TabContainer({
         style: "height: 100%; width: 100%;",
 		tabPosition: "left-h"
     }, "tc1-prog");
 
     var cp1 = new ContentPane({
-         title: "Run",
-         content: "We offer amazing food"
+         title: "Run"
     });
+	cp1.addChild(textarea);
+	cp1.addChild(runButton);
     tc.addChild(cp1);
 
     var cp2 = new ContentPane({
